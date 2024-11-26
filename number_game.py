310fa5b001b4dbec900fd46e9866e80f2f8d17a4
CalvinGaiennie <calvingaiennie@gmail.com>
2024-11-26 16:23:05 -0600

    idk

--- number_game.py
+++ number_game.py
@@ -1,15 +1,29 @@
 import random 
+
+for _ in range(10):
+    print("\n")
+    
 random_number = random.randint(1,10)
-print(random_number)
+print('Random Number',random_number)
 guesses = []
 game_over = 0
-guess = input("I'm thinking of a number between 1 and 100. You have five guesses. Begin by typing your first guess.")
+guess = -1
+
+def checkGuess(guesss):
+    global game_over
+    current_guess = int(guesss)
+    if current_guess != random_number:
+        print('wrong')
+    elif current_guess == random_number:
+        print('Correct you win!')
+        game_over = 1
+
 
 while game_over != 1:
-    if guess != random_number:
-        print('wrong')
-        guess = input('Guess again')
-
-if guess == random_number:
-    print('Correct')
-    game_over = 1
+    if len(guesses) == 0:
+        guess = input("I'm thinking of a number between 1 and 100. You have five guesses. Begin by typing your first guess.")
+        checkGuess(guess)
+    else:
+        guess = input("Guess again.")
+        checkGuess(guess)
+    guesses.append(guess)
