d9dd14c6ba1f2fcfbe5a5add38dace55ee41de91
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-29 15:26:32 -0600

    udemy course

---
+++ OOP/main.py
@@ -0,0 +1,24 @@
+# from turtle import Turtle, Screen
+
+# import another_module
+
+# print(another_module.another_variable)
+
+
+# timmy = Turtle()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+# timmy.left(30)
+# timmy.forward(100)
+
+
+
+# my_screen.exitonclick()
+
+
+
